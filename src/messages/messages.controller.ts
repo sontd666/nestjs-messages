7e49c28dcae1f6f4895dbf27b317c0bbc5615271
Thai Doan Son <thai.doan.son@azoom.jp>
2025-01-22 13:18:54 +0700

    :+1: Add validation

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
@@ -11,7 +12,7 @@ export class MessagesController {
   }
 
   @Post()
-  createMessage(@Body() body: any) {
+  createMessage(@Body() body: CreateMessageDto) {
     console.log('body', body);
   }
 }
